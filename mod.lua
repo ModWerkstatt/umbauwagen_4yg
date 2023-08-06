8c8bcd63d2ed94b01fb86b92174b44f0514fe09f
Florian Thiel <flth@flth.de>
2023-08-06 21:30:29 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "germany", "umbauwagen", "wagon", "passagenger", "bundesbahn",  },
+	},
+	options = {
+	},
+}
+end
